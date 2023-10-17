d73560e30637c793ca53c4cf396727d4f7ab39a1
RomanRazdorov <danka.kuzka@mail.ru>
2023-10-17 19:26:01 +0300

    Committing programming war crimes 3

--- app/src/main/java/com/example/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeapp/MainActivity.kt
@@ -3,49 +3,58 @@ package com.example.jetpackcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.Text
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.jetpackcomposeapp.ui.theme.JetpackComposeAppTheme
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import kotlinx.coroutines.launch
+import androidx.compose.runtime.*
+
+
 
 data class Student(val name: String, val group: String)
 
-private val textStyle =
+enum class Screen {
+    Home,
+    Menu
+}
 
+private val textStyle =
     TextStyle(
         fontFamily = FontFamily.SansSerif,
         fontSize = 18.sp,
         fontWeight = FontWeight.Bold,
         color = Color.Black
     )
-// Другие стили
-/*
-   TextStyle(
-       fontFamily = FontFamily.Serif,
-       fontSize = 21.sp,
-       fontWeight = FontWeight.Light,
-       color = Color.Magenta
-   )
-   */
-
 
 @Composable
 fun StudentInfo(studentName: String, group: String) {
@@ -66,27 +75,152 @@ fun StudentInfo(studentName: String, group: String) {
 }
 
 @Composable
-fun MyApp(students: List<Student>) {
-    MaterialTheme(
-        typography = Typography(
-            TextStyle(
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold
+fun StudentListScreen(students: List<Student>, navigateToDetail: (Student) -> Unit) {
+    // Экран со списком студентов
+    // Можно использовать LazyColumn для отображения списка студентов
+    LazyColumn {
+        items(students) { student ->
+            // Нажатие на студента должно вызвать переход на экран деталей
+            Text(
+                text = "ФИО: ${student.name}\nГруппа: ${student.group}",
+                style = textStyle,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clickable { navigateToDetail(student) }
             )
-        ),
-        content = {
-            Surface(
-                color = MaterialTheme.colorScheme.background,
-                modifier = Modifier.fillMaxSize()
+        }
+    }
+}
+
+@Composable
+fun DrawerItem(
+    screen: Screen,
+    currentScreen: Screen,
+    onClick: (Screen) -> Unit
+) {
+    Text(
+        text = stringResource(id = R.string.screen_home),
+        style = textStyle,
+        modifier = Modifier
+            .padding(16.dp)
+            .clickable {
+                if (screen != currentScreen) {
+                    onClick(screen)
+                }
+            }
+    )
+}
+
+@Composable
+fun DrawerContent(
+    currentScreen: Screen,
+    onScreenSelected: (Screen) -> Unit
+) {
+    Column {
+        DrawerItem(
+            screen = Screen.Home,
+            currentScreen = currentScreen,
+            onClick = onScreenSelected
+        )
+        DrawerItem(
+            screen = Screen.Menu,
+            currentScreen = currentScreen,
+            onClick = onScreenSelected
+        )
+    }
+}
+
+
+@Composable
+fun StudentDetailScreen(student: Student) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "ФИО: ${student.name}\nГруппа: ${student.group}",
+            style = textStyle,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun MyApp(students: List<Student>) {
+    val navController = rememberNavController()
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    var currentScreen by remember { mutableStateOf<Screen>(Screen.Home) }
+
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Список студентов")
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                backgroundColor = Color.Blue
             ) {
-                LazyColumn {
-                    items(students) { student ->
-                        StudentInfo(student.name, student.group)
+
+                IconButton(
+                    onClick = {
+                        scope.launch {
+                            scaffoldState.drawerState.open()
+                        }
+                    }
+                ) {
+                    Icon(imageVector = Icons.Default.Menu, contentDescription = "Меню")
+                }
+            }
+        },
+        drawerContent = {
+            DrawerContent(
+                currentScreen = currentScreen,
+                onScreenSelected = { newScreen ->
+                    currentScreen = newScreen // Обновляем currentScreen с использованием mutableStateOf
+                    scope.launch {
+                        scaffoldState.drawerState.close()
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "studentList"
+        ) {
+            composable("studentList") {
+                StudentListScreen(students) { student ->
+                    navController.navigate("studentDetail/${student.name}/${student.group}")
+                }
+            }
+            composable(
+                "studentDetail/{studentName}/{group}",
+                arguments = listOf(
+                    navArgument("studentName") { type = NavType.StringType },
+                    navArgument("group") { type = NavType.StringType }
+                )
+            ) { backStackEntry ->
+                val studentName = backStackEntry.arguments?.getString("studentName")
+                val group = backStackEntry.arguments?.getString("group")
+                if (studentName != null && group != null) {
+                    val student = students.find { it.name == studentName && it.group == group }
+                    if (student != null) {
+                        StudentDetailScreen(student)
                     }
                 }
             }
         }
-    )
+    }
 }
 
 class MainActivity : ComponentActivity() {
